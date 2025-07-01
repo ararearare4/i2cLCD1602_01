bbf6df567a35e801890889a8bb66eb4b7feae2e4
ararearare4 <51278141+ararearare4@users.noreply.github.com>
2025-07-02 07:04:38 +0900

    mainに変える

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@
 /**
  * Custom blocks
  */
-//% weight=20 color=#1E90FF" icon="▀"
+//% weight=20 color="#1E90FF" icon="▀"
 namespace I2C_LCD1602_KANA {
     let i2cAddr: number // 0x3F: PCF8574A, 0x27: PCF8574
     let BK: number      // backlight control
